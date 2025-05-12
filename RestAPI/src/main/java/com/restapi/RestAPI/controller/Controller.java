6f5630f7aae5e9db93fbb38abcfdf1907ec9e569
João Victor Seide <seidejvs@gmail.com>
2025-05-11 22:09:34 -0300

    criei os métodos para criar e listar objetos

--- RestAPI/src/main/java/com/restapi/RestAPI/controller/Controller.java
+++ RestAPI/src/main/java/com/restapi/RestAPI/controller/Controller.java
@@ -1,6 +1,13 @@
 package com.restapi.RestAPI.controller;
 
+import com.restapi.RestAPI.model.Model;
 import com.restapi.RestAPI.service.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +16,15 @@ public class Controller {
 
     public Controller(Service service) {
         this.service = service;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@Validated @RequestBody Model model){
+        return new ResponseEntity<Model>(service.create(model), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<?> list(){
+        return new ResponseEntity<>(service.list(), HttpStatus.OK);
     }
 }
--- RestAPI/src/main/java/com/restapi/RestAPI/service/Service.java
+++ RestAPI/src/main/java/com/restapi/RestAPI/service/Service.java
@@ -2,8 +2,11 @@ package com.restapi.RestAPI.service;
 
 import com.restapi.RestAPI.model.Model;
 import com.restapi.RestAPI.repository.Repository;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 @org.springframework.stereotype.Service
 public class Service {
@@ -15,12 +18,21 @@ public class Service {
 
     public Model create(Model model){
         if(model == null){
-            throw new IllegalArgumentException("Ta vazio isso ai man");
+            throw new IllegalArgumentException("Ta vazio isso ai man!");
         }
         if(model.getAutor().isBlank() || model.getTitulo().isBlank() || model.getGenero().isBlank()){
-            throw new IllegalArgumentException("Ta errado os atributo nisso ai man");
+            throw new IllegalArgumentException("Ta errado os atributo nisso ai man!");
         }
 
         return repository.save(model);
+    }
+
+    public Iterable<Model> list(){
+        List<Model> list = repository.findAll();
+        if(list.isEmpty()){
+            throw new EmptyResultDataAccessException("Nenhum registro encontrado!", 1);
+        }
+
+        return repository.findAll();
     }
 }
